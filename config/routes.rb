0c657ba0b2a00042d3e4627a030c9578e4c01976
Tetsurafu <tetsurafu@gmail.com>
2019-04-23 15:27:00 +0900

    Prep

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'messages#index'
+  # 仮置きのルーティングとして、messages_controllerのindexアクションがルートパスになるようにしてください。
 end
+
+# テスト
+# テスト2

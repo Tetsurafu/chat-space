f5ed59704b27de8552271406da7242d5c8641c50
Tetsurafu <tetsurafu@gmail.com>
2019-05-07 17:50:55 +0900

    devise is missing

---
+++ db/migrate/20190507073219_create_group_users.rb
@@ -0,0 +1,10 @@
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_users do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.string   :group_name
+      t.timestamps
+    end
+  end
+end

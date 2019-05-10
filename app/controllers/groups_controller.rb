41eefec78019ad882ff291843ab65bf23e0f0444
Tetsurafu <tetsurafu@gmail.com>
2019-05-10 10:41:57 +0900

    Finished Group 6,7

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,27 @@
 class GroupsController < ApplicationController
+  protect_from_forgery except: :create
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-  end
-
-  def edit
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def update
   end
-  
+
+  def index
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => []})
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,17 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
+  validates :name, presence: true
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, user_ids: [])
+  end
+
 end

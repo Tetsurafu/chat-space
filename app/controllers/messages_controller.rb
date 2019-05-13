302ca7efdfd93cded15dd49576326e69c6bdbe8e
Tetsurafu <tetsurafu@gmail.com>
2019-05-13 16:08:49 +0900

    Before installing carrierwave

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,22 @@
 class MessagesController < ApplicationController
   
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
+    @message = @group.messages.create(message_params)
+      redirect_to group_messages_path(@group)
   end
-  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
